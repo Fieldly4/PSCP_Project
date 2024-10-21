bc3236ea84a930c6d669c86462fb6afd8a59d94d
Fieldly4 <67070040@kmitl.ac.th>
2024-10-21 20:25:21 +0700

    Add Js
    ทำไมมันแยกไฟล์แล้วรันไม่ไปหว่าาาา

---
+++ Electricity/templates/script.js
@@ -0,0 +1,8 @@
+let elect_name = document.getElementById("name_electrical");
+let resulted = document.getElementById("Resulted");
+
+resulted.textContent = "Test";
+
+function clicked(){
+    console.log(elect_unit.value)
+};
