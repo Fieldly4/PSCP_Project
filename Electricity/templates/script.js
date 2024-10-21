264f636ef93e5b550691514f94ccb1d91529f4b0
Fieldly4 <67070040@kmitl.ac.th>
2024-10-22 00:34:48 +0700

    Delete Electricity/templates directory
    ลบอันเกิน

--- Electricity/templates/script.js
+++
@@ -1,8 +0,0 @@
-let elect_name = document.getElementById("name_electrical");
-let resulted = document.getElementById("Resulted");
-
-resulted.textContent = "Test";
-
-function clicked(){
-    console.log(elect_unit.value)
-};
