e327496821fffb06ed5d73a9b5721cd81af41501
ManassanunK <67070142@kmitl.ac.th>
2024-11-01 15:41:08 +0700

    Add files via upload
    ปรับแต่งให้สวย เพิ่มTHB เพิ่มโลโก้

---
+++ Electricity/web.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+web = Flask(__name__)
+
+
+@web.route('/')
+def index():
+    return render_template("electric.html")
+
+
+if __name__ == "__main__":
+    web.run(debug=True)
